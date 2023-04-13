56331533a5ba2e4168b2073cf2a434077200309d
Vemuri Bhavana <82796465+Vemuri-Bhavana@users.noreply.github.com>
2023-04-13 18:15:39 +0530

    Add files via upload

---
+++ network/chaincode/files/registerBuyer.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const { Wallets } = require('fabric-network');
+const FabricCAServices = require('fabric-ca-client');
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter enrollment id: ', async (eid) => {
+    console.log(`You entered: ${eid}`);
+
+    try {
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, '..', '..', 'organizations', 'peerOrganizations', 'buyer.example.com', 'connection-buyer.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new CA client for interacting with the CA.
+        const caURL = ccp.certificateAuthorities['ca.buyer.example.com'].url;
+        const ca = new FabricCAServices(caURL);
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userIdentity = await wallet.get(eid);
+        if (userIdentity) {
+            console.log(`An identity for the user ${eid} already exists in the wallet`);
+            return;
+        }
+
+        // Check to see if we've already enrolled the admin user.
+        const adminIdentity = await wallet.get('buyer_admin');
+        if (!adminIdentity) {
+            console.log(`An identity for the buyer_admin user ${eid} does not exist in the wallet`);
+            console.log(`Run the enrollAdmin.js application before retrying`);
+            return;
+        }
+
+        // build a user object for authenticating with the CA
+        const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+        const adminUser = await provider.getUserContext(adminIdentity, 'buyer_admin');
+
+        // Register the user, enroll the user, and import the new identity into the wallet.
+        const secret = await ca.register({
+            enrollmentID: eid,
+            role: 'client'
+        }, adminUser);
+        const enrollment = await ca.enroll({
+            enrollmentID: eid,
+            enrollmentSecret: secret
+        });
+        const x509Identity = {
+            credentials: {
+                certificate: enrollment.certificate,
+                privateKey: enrollment.key.toBytes(),
+            },
+            mspId: 'BuyerMSP',
+            type: 'X.509',
+        };
+        await wallet.put(eid, x509Identity);
+        console.log(`Successfully registered and enrolled buyer_admin use ${eid} and imported it into the wallet`);
+
+    } catch (error) {
+        console.error(`Failed to register buyer ${eid}: ${error}`);
+        process.exit(1);
+    }
+    rl.close();
+});
---
+++ network/chaincode/files/registerLogistics.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const { Wallets } = require('fabric-network');
+const FabricCAServices = require('fabric-ca-client');
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter enrollment id: ', async (eid) => {
+    console.log(`You entered: ${eid}`);
+
+    try {
+        // load the network configuration
+        const ccpPath = path.resolve(__dirname, '..', '..', 'organizations', 'peerOrganizations', 'logistics.example.com', 'connection-logistics.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+        // Create a new CA client for interacting with the CA.
+        const caURL = ccp.certificateAuthorities['ca.logistics.example.com'].url;
+        const ca = new FabricCAServices(caURL);
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const userIdentity = await wallet.get(eid);
+        if (userIdentity) {
+            console.log(`An identity for the user ${eid} already exists in the wallet`);
+            return;
+        }
+
+        // Check to see if we've already enrolled the admin user.
+        const adminIdentity = await wallet.get('logistics_admin');
+        if (!adminIdentity) {
+            console.log(`An identity for the logistics_admin user ${eid} does not exist in the wallet`);
+            console.log(`Run the enrollAdmin.js application before retrying`);
+            return;
+        }
+
+        // build a user object for authenticating with the CA
+        const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+        const adminUser = await provider.getUserContext(adminIdentity, 'logistics_admin');
+
+        // Register the user, enroll the user, and import the new identity into the wallet.
+        const secret = await ca.register({
+            enrollmentID: eid,
+            role: 'client'
+        }, adminUser);
+        const enrollment = await ca.enroll({
+            enrollmentID: eid,
+            enrollmentSecret: secret
+        });
+        const x509Identity = {
+            credentials: {
+                certificate: enrollment.certificate,
+                privateKey: enrollment.key.toBytes(),
+            },
+            mspId: 'LogisticsMSP',
+            type: 'X.509',
+        };
+        await wallet.put(eid, x509Identity);
+        console.log(`Successfully registered and enrolled admin use ${eid} and imported it into the wallet`);
+
+    } catch (error) {
+        console.error(`Failed to register logistics ${eid}: ${error}`);
+        process.exit(1);
+    }
+
+    rl.close();
+});
